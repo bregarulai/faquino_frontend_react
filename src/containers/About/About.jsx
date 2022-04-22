5012a6bd863208bed621768a007ab7209325626c
bregarulai <bregarulai@gmail.com>
2022-04-22 11:20:10 -0400

    connected about section with sanity backend

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -2,32 +2,40 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import './about.scss';
+import { urlFor, client } from '../../client';
 import { images } from '../../constants';
 
-const abouts = [
-  {
-    title: 'Web Development',
-    description: 'I am a good web developer',
-    imgUrl: images.about01,
-  },
-  {
-    title: 'Frontend Developer',
-    description: 'I am a good web designer',
-    imgUrl: images.about02,
-  },
-  {
-    title: 'Backend Develooper',
-    description: 'I am a good web UX/UI developer',
-    imgUrl: images.about03,
-  },
-  {
-    title: 'MERN Stack',
-    description: 'I am a good in web developement animations',
-    imgUrl: images.about04,
-  },
-];
+// const abouts = [
+//   {
+//     title: 'Web Development',
+//     description: 'I am a good web developer',
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: 'Frontend Developer',
+//     description: 'I am a good web designer',
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: 'Backend Develooper',
+//     description: 'I am a good web UX/UI developer',
+//     imgUrl: images.about03,
+//   },
+//   {
+//     title: 'MERN Stack',
+//     description: 'I am a good in web developement animations',
+//     imgUrl: images.about04,
+//   },
+// ];
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className='head-text'>
@@ -42,7 +50,7 @@ const About = () => {
             className='about__profile'
             key={`${about.title}${index}`}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>
               {about.title}
             </h2>
