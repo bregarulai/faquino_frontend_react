8ddc72b4ed6b2575cd22cb8ce192d275267239b1
bregarulai <bregarulai@gmail.com>
2022-04-21 20:54:00 -0400

    created mobile menu for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './navbar.scss';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className='navbar'>
       <div className='navbar__logo'>
@@ -17,6 +20,24 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <div className='navbar__menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item} onClick={() => setToggle(false)}>
+                  <a href={`#${item}`}>{item}</a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
