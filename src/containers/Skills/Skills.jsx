a560df7cc07fac073f18d8e27afc65e0ccf9511f
bregarulai <bregarulai@gmail.com>
2022-04-25 17:23:57 -0400

    styled experience section for skills component

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ReactToolTip from 'react-tooltip';
+import ReactTooltip from 'react-tooltip';
 import { motion } from 'framer-motion';
 
 import './skills.scss';
@@ -21,12 +21,12 @@ const Skills = () => {
       <h2 className='head-text'>Skills and Experience</h2>
       <div className='skills__container'>
         <motion.div className='skills__list'>
-          {skills.map((skill) => (
+          {skills.map((skill, index) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className='skills__item app__flex'
-              key={skill.name}
+              key={skill.name + index}
             >
               <div
                 className='app__flex'
@@ -38,40 +38,40 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
-        <motion.div className='skills__exp'>
-          {experiences.map((experience) => (
-            <motion.div key={experience.year} className='skills__exp-item'>
+        <div className='skills__exp'>
+          {experiences.map((experience, index) => (
+            <div key={experience.year + index} className='skills__exp-item'>
               <div className='skills__exp-year'>
                 <p className='bold-text'>{experience.year}</p>
               </div>
-              <motion.div className='skills__exp-works'>
-                {experience.works.map((work) => (
-                  <>
-                    <motion.div
-                      whileInView={{ opacity: [0, 1] }}
-                      transition={{ duration: 0.5 }}
-                      className='skills__exp-work'
-                      key={work.name}
-                      data-tip
-                      data-for={work.name}
-                    >
-                      <h4 className='bold-text'>{work.name}</h4>
-                      <p className='p-text'>{work.company}</p>
-                    </motion.div>
-                    <ReactToolTip
-                      id={work.name}
-                      effect='solid'
-                      arrowColor='#fff'
-                      className='skills-tooltip'
-                    >
-                      {work.desc}
-                    </ReactToolTip>
-                  </>
-                ))}
-              </motion.div>
-            </motion.div>
+              {experience.works.map((work, index) => (
+                <motion.div
+                  className='skills__exp-works'
+                  key={work.name + index}
+                >
+                  <motion.div
+                    whileInView={{ opacity: [0, 1] }}
+                    transition={{ duration: 0.5 }}
+                    className='skills__exp-work'
+                    data-tip
+                    data-for={work.name}
+                  >
+                    <h4 className='bold-text'>{work.name}</h4>
+                    <p className='p-text'>{work.company}</p>
+                  </motion.div>
+                  <ReactTooltip
+                    id={work.name}
+                    effect='solid'
+                    arrowColor='#fff'
+                    className='skills-tooltip'
+                  >
+                    {work.desc}
+                  </ReactTooltip>
+                </motion.div>
+              ))}
+            </div>
           ))}
-        </motion.div>
+        </div>
       </div>
     </>
   );
