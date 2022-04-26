72fd83af0fcd56ff204c252a62701e4dfe7cdb63
bregarulai <bregarulai@gmail.com>
2022-04-25 20:08:35 -0400

    fetching testimonials data and wrapped component with app and motion wrapper

--- src/containers/Testimonial/Testimonial.jsx
+++ src/containers/Testimonial/Testimonial.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
 import './testimonial.scss';
+import { AppWrapper, MotionWrapper } from '../../wrapper';
+import { urlFor, client } from '../../client';
 
 const Testimonial = () => {
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const brandsQuery = '*[_type == "brands"]';
+    const testimonialsQuery = '*[_type == "testimonials"]';
+    client.fetch(brandsQuery).then((data) => setBrands(data));
+    client.fetch(testimonialsQuery).then((data) => setTestimonials(data));
+  }, []);
   return <div>Testimonial</div>;
 };
 
-export default Testimonial;
+export default AppWrapper(
+  MotionWrapper(Testimonial, 'testimonials'),
+  'testimonials',
+  'app__primarybg'
+);
